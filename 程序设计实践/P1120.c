bc10c9a30af0fdbe8f56971285c8aefdf6d3cec4
JohnLu-HP-Laptop <lzh20020204@126.com>
2021-04-04 23:58:08 +0800

    Update P1120.c

--- 程序设计实践/P1120.c
+++ 程序设计实践/P1120.c
@@ -5,14 +5,15 @@ int main(){
 	int la,lb,l,i,p,c[201];
 	while(~scanf("%s%s",&a,&b)){
 		la=strlen(a);lb=strlen(b);
-		i=0;p=0;
-		for(l=la>lb?la:lb;i<=l;i++){
-			if(i<la)p+=(int)a[la-i-1]-'0';
-			if(i<lb)p+=(int)b[lb-i-1]-'0';
+		p=0;
+		l=la>lb?la:lb;
+		for(i=0;i<=l;i++){
+			if(i<la)p+=a[la-i-1]-'0';
+			if(i<lb)p+=b[lb-i-1]-'0';
 			c[i]=p%10;
 			p/=10;
 		}
-		for(i=l;c[i]==0&i>0;i--);
+		for(i=l;c[i]==0&&i>0;i--);
 		for(;i>=0;i--)printf("%d",c[i]);
 		printf("\n");
 	}
