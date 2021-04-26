5dcc59781997c9196872e1c9a1f73ba75bc3b2c7
JohnLu-HP-Laptop <lzh20020204@126.com>
2021-04-26 17:23:31 +0800

    Update P1121.c

--- 程序设计实践/P1121.c
+++ 程序设计实践/P1121.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char a[201],b[201],t[201];
-	int la,lb,c[201]={0},p=0,i;
-	while(~scanf("%s %s",a,b)){
+	char a[201],b[201];
+	int la,lb,c[201],p=0,i;
+	while(~scanf("%s%s",a,b)){
 		la=strlen(a);lb=strlen(b);
-		for(i=0;i<201;++i)
 			c[i]=0;
-		// if(la<lb||(la==lb&&strcmp(a,b)<0)){
-		// 	printf("-");
-		// 	strcpy(t,a);
-		// 	strcpy(a,b);
-		// 	strcpy(b,t);
-		// 	la+=lb;
-		// 	lb=la-lb;
-		// 	la-=lb;
-		// }
-		for(i=0;i<=la;i++){
-			if(i<=lb) c[i]=a[la-i]-b[lb-i]-p;
-			else c[i]=a[la-i]-'0'-p;
+		for(i=0;i<201;++i)c[i]=0;
+		for(i=1;i<=la;i++){
+			if(i<=lb)c[i]=a[la-i]-b[lb-i]-p;
+			else	c[i]=a[la-i]-'0'-p;
 			if(c[i]<0){
 				p=1;
 				c[i]+=10;
